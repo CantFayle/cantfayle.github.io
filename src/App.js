59fbcb883105bce540c8b37af855c93ce9d0c4f7
Conor Fayle <conor.fayle@gmail.com>
2024-02-13 15:31:24 +0000

    build homepage

--- src/App.js
+++ src/App.js
@@ -2,11 +2,35 @@ import './App.css';
 import Calculator from './Calculator';
 
 function App() {
+  const navItems = ['Home', 'Experience', 'Projects', 'Contact'];
   return (
-    <div className="App">
-      <header className="App-header" style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-        <Calculator/>
-      </header>
+    <div className="page">
+      <div className="frame">
+        <div className="container">
+          <div className="name">
+            Conor Fayle
+          </div>
+          <div className="description">
+            Software Developer
+          </div>
+          <div className="content">
+            <div className="navigation">
+              {navItems.map(navItem =>
+                <a className="nav-item" href="#">
+                  {navItem}
+                </a>
+              )}
+            </div>
+            <div className="section">{`
+              An experienced software developer with a proven track record of delivery.
+              \n
+              Comfortable standing up to share my opinion, and an advocate for personal growth and positive change.
+              \n
+              Under all that, a person who just loves a puzzle.
+            `}</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
