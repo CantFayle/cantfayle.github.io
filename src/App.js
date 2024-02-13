9e16530c80a48386d4cb3df9b4b29156d14d9164
Conor Fayle <conor.fayle@gmail.com>
2024-02-13 18:10:10 +0000

    add navigation and qr code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
+import React, { useState } from 'react';
 import './App.css';
+import qr from './qr-code.svg';
 import Calculator from './Calculator';
 
+const navItems = [
+  {
+    title: 'Home',
+    content: () => `
+      An experienced software developer with a proven track record of delivery.
+      \n
+      Comfortable standing up to share my opinion, and an advocate for personal growth and positive change.
+      \n
+      Under all that, a person who just loves a puzzle.
+    `
+  },
+  {
+    title: 'Experience',
+    content: () => `More to come...`
+  },
+  {
+    title: 'Projects',
+    content: () => `More to come...`
+  },
+  {
+    title: 'Contact',
+    content: () =>
+      <>
+        <img src={qr} className="qr" alt={"QR"}/>
+      </>
+  },
+];
+
 function App() {
-  const navItems = ['Home', 'Experience', 'Projects', 'Contact'];
+  const getNav = navTitle => navItems.find(({ title }) => title === navTitle);
+  const [selectedNav, setSelectedNav] = useState(getNav('Home'))
   return (
     <div className="page">
       <div className="frame">
@@ -16,18 +47,17 @@ function App() {
           <div className="content">
             <div className="navigation">
               {navItems.map(navItem =>
-                <a className="nav-item" href="#">
-                  {navItem}
-                </a>
+                <div
+                  className={`nav-item ${navItem.title === selectedNav.title ? 'nav-item-selected' : ''}`}
+                  onClick={() => setSelectedNav(navItem)}
+                >
+                  {navItem.title}
+                </div>
               )}
             </div>
-            <div className="section">{`
-              An experienced software developer with a proven track record of delivery.
-              \n
-              Comfortable standing up to share my opinion, and an advocate for personal growth and positive change.
-              \n
-              Under all that, a person who just loves a puzzle.
-            `}</div>
+            <div className="section">
+              {selectedNav.content()}
+            </div>
           </div>
         </div>
       </div>
